faa2fb229a3c5a6507f361a3a33e7f6b154fa70d
biaandersson <expr.tba@gmail.com>
2020-11-05 22:01:23 +0100

    logout created

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -6,15 +6,13 @@ import { SignUpLink } from '../SignUp';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
-const SignInPage = () => {
-  return (
-      <div>
-        <h1>SignIn</h1>
-        <SignInForm/>
-        <SignUpLink/>
-      </div>
-  );
-};
+const SignInPage = () => (
+    <div>
+      <h1>SignIn</h1>
+      <SignInForm/>
+      <SignUpLink/>
+    </div>
+);
 
 const INITIAL_STATE = {
   email: '',
@@ -29,7 +27,7 @@ class SignInFormBase extends Component {
     this.state = { ...INITIAL_STATE };
   }
 
-  onSubmit = (event) => {
+  onSubmit = event => {
     const { email, password } = this.state;
 
     this.props.firebase
@@ -37,18 +35,22 @@ class SignInFormBase extends Component {
         .then(() => {
           this.setState({ ...INITIAL_STATE });
           this.props.history.push(ROUTES.HOME);
-        }).catch(error => this.setState({ error }));
+        })
+        .catch(error => {
+          this.setState({ error });
+        });
 
     event.preventDefault();
   };
 
-  onChange = (event) => {
+  onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
   render() {
     const { email, password, error } = this.state;
-    const isInvalid = password === '' || email !== '';
+
+    const isInvalid = password === '' || email === '';
 
     return (
         <form onSubmit={this.onSubmit}>
@@ -69,6 +71,7 @@ class SignInFormBase extends Component {
           <button disabled={isInvalid} type="submit">
             Sign In
           </button>
+
           {error && <p>{error.message}</p>}
         </form>
     );
